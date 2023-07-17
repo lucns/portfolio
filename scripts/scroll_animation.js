55eb6efd695f738fc53c5fa877f9e3d245c64987
lucns <lucns2906@gmail.com>
2023-07-16 21:37:27 -0300

    Add files via upload

--- scripts/scroll_animation.js
+++ scripts/scroll_animation.js
@@ -3,19 +3,22 @@ body.style.backgroundColor = '#121212';
 var star = document.getElementById('star');
 var button = document.getElementById('button');
 var textDeveloper = document.getElementById('text_developer');
+var iImage = document.getElementById('image_banner');
 
 // trigger this function every time the user scrolls
 window.onscroll = function (event) {
 	var scroll = window.pageYOffset;
+	console.log("scroll " + scroll);
 	var scale = 1 + ((scroll * 2) / 1000);
 	var percentage = (scale - 1) * 50;
 	if (scroll < 10) {
 		textDeveloper.style.opacity = "1";
+		iImage.style.opacity = "1";
 	} else {
 		var alpha = 1 - (scroll / 600);
 		if (alpha < 0) alpha = 0;
 		textDeveloper.style.opacity = "" + alpha;
-		//console.log("alpha " + alpha);
+		iImage.style.opacity = "" + (alpha / (scroll / 20));
 	} 
 	
 	if (scroll < 300) {
@@ -24,12 +27,12 @@ window.onscroll = function (event) {
 		//star.style.transform = 'scale(' + scale + ')';
 		//star.style.transform = 'translate(' + percentage + '%, ' + '0%)';
 		//star.style.transition= " all 500ms";
-	} else if (scroll >= 600 && scroll < 1200) {
-		body.style.backgroundColor = '#242424';
+	} else if (scroll >= 500 && scroll < 1200) {
+		//body.style.backgroundColor = '#242424';
 	} else if (scroll >= 1200 && scroll < 1800) {
-		body.style.backgroundColor = '#484848';
+		body.style.backgroundColor = '#181818';
 	} else if (scroll >= 1800 && scroll < 3000) {
-		body.style.backgroundColor = '#aaaaaa';
+		body.style.backgroundColor = '#242424';
 	}
 	if (scale != 1) {
 		button.style.transform = 'translate(-180%, 0%)';
@@ -42,5 +45,9 @@ window.onscroll = function (event) {
 		star.style.transform = 'translate(0%, 0%)';
 		star.style.transition= " all 500ms";
 	}
-	textDeveloper.style.transform = 'translateY(' + scroll + 'px)';
+	if (scroll <= 200) {
+		textDeveloper.style.transform = 'translateY(-' + scroll + 'px)';
+	} else {
+		//textDeveloper.style.transform = 'translate(0%, 0%)';
+	}
 }
